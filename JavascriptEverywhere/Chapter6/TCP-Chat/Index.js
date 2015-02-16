d0614f6facbc4d8eb142583ebe036d485683f907
Xiaoxi Song <xiaoxi.song85@gmail.com>
2015-02-16 12:58:39 +0800

    complete the chapter6 code

--- JavascriptEverywhere/Chapter6/TCP-Chat/Index.js
+++ JavascriptEverywhere/Chapter6/TCP-Chat/Index.js
@@ -1,5 +1,6 @@
 var net = require("net");
-var count = 0;
+var count = 0,
+    users = {};
 var server = net.createServer(function(conn){
     console.log('\033[90m   new connection!\033[39m');
     conn.write(
@@ -8,14 +9,40 @@ var server = net.createServer(function(conn){
         + '\r\n > please write your nikcname and press enter: '
         );
     count++;
+    var nickname;
     conn.setEncoding('utf8');
+
+    function broadcast(msg, exceptMyself){
+        for(var i in users){
+            if(!exceptMyself || i != nickname){
+                users[i].write(msg);
+            }
+        }
+    }
 
     conn.on('close', function(){
         count--;
+        delete users[nickname];
+        broadcast('\033[90m > ' + nickname + ' left the room\033[39m\n');
     });
 
     conn.on('data', function(data){
         console.log(data);
+        data = data.replace('\r\n', '');
+        if(!nickname){
+            if(users[data]){
+                conn.write('\033[93m> nickname already in use. try again:\033[39m ');
+                return;
+            }
+            else{
+                nickname = data;
+                users[nickname] = conn;
+                broadcast('\033[90m > ' + nickname + ' joined the room\033[39m\n');
+                }
+            }
+        else{
+             broadcast('\033[96m> ' + nickname + ':\033[39m ' + data + '\n', true);
+            }
     });
 });
 
